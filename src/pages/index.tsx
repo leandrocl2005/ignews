e2d0c6218d1ed03b565be601c23253315d1aee00
leandrocl2005 <leandrocl2005@yahoo.com.br>
2021-03-26 15:26:56 -0300

    project setup

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+import styles from '../styles/home.module.scss';
+
+export default function Home() {
+  return (
+    <>
+      <Head><title>IG news</title></Head>
+      <h1>
+        Hello, world!
+      </h1>
+    </>
+  );
+}
